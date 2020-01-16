26e7a29464c29e12ef37d431e9d1da9eb281b964
Camamber <egorka.k2009@gmail.com>
2020-01-16 14:08:26 +0200

    startup

---
+++ mariadb/docker-entrypoint-initdb.d/pmm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pmm-admin config --server pmm-server:80
+#pmm-admin add linux:metrics
+#pmm-admin add mysql:metrics --host mariadb --user pmm --password expo213 
+#pmm-admin add mysql:queries --host mariadb --user pmm --password expo213 --query-source perfschema
