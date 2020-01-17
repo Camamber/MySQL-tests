d06a6a4771ea7c494cf7f6851c813bb90ca98f8a
Camamber <egorka.k2009@gmail.com>
2020-01-17 15:26:59 +0200

    fix backups script

--- backup.sh
+++ backup.sh
@@ -1,3 +1,3 @@
 BACKUP_NAME=mysql_$(date -u +%Y-%m-%dT%H-%M-%S)
-/usr/local/bin/docker-compose run --rm mysql-backup2 create $BACKUP_NAME
-/usr/local/bin/docker-compose run --rm mysql-backup2 upload $BACKUP_NAME
+/usr/local/bin/docker-compose run --rm mysql-backup create $BACKUP_NAME
+/usr/local/bin/docker-compose run --rm mysql-backup upload $BACKUP_NAME
